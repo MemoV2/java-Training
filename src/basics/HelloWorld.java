a88453f621ae11a04ccddcfcba4c38cf09b7f1d7
MemoV2 <guillermorenteria85@gmail.com>
2018-08-27 12:53:58 -0500

    Modified helloworld app

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -2,6 +2,7 @@ package basics;
 
 public class HelloWorld {
 	public static void main (String[] args){
-		System.out.println("Hello World, Perro");
+		System.out.println("Hello World");
+		System.out.println("I'm adding code to my project");
 	}
 }
